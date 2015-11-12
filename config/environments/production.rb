9249a0833d4fa9706482dd5b8183fdab7c9315c0
Itay Nussbaum <itay.n@fiverr.com>
2015-11-12 12:56:28 +0200

    gem 'rails_12factor' heroku 5

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,5 +66,5 @@ SampleApp::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
-  # config.assets.initialize_on_precompile = true
+  config.assets.initialize_on_precompile = true
 end
